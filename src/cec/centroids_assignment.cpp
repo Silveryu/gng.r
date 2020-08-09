32cf7fe06d0f0dd95e28e324fee4d8dfb9c482dd
Silveryu <p.m.s@ua.pt>
2020-08-09 11:51:55 +0100

    delete gmum.r classes not related to Approximate GNG

--- src/cec/centroids_assignment.cpp
+++
@@ -1,9 +0,0 @@
-#include "centroids_assignment.hpp"
-
-namespace gmum {
-
-void CentroidsAssignment::operator()(std::vector<unsigned int> &assignment) {
-	assign_points(assignment, m_centroids, m_points);
-}
-
-}
